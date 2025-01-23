73c0fb65e7f44654a8f03bd020335bfa5a9394cd
flt6 <1404262047@qq.com>
2025-01-23 23:57:49 +0800

    remove old

--- components/Live2D.js
+++ components/Live2D.js
@@ -9,50 +9,42 @@ import { useEffect } from 'react'
  * @returns
  */
 export default function Live2D() {
-  const { theme, switchTheme } = useGlobal()
-  const showPet = JSON.parse(siteConfig('WIDGET_PET'))
-  const petLink = siteConfig('WIDGET_PET_LINK')
-  const petSwitchTheme = siteConfig('WIDGET_PET_SWITCH_THEME')
+  // const { theme, switchTheme } = useGlobal()
+  // const showPet = JSON.parse(siteConfig('WIDGET_PET'))
+  // const petLink = siteConfig('WIDGET_PET_LINK')
+  // const petSwitchTheme = siteConfig('WIDGET_PET_SWITCH_THEME')
 
-  useEffect(() => {
-    if (showPet && !isMobile()) {
-      Promise.all([
-        loadExternalResource(
-          'https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js',
-          'js'
-        )
-      ]).then(e => {
-        if (typeof window?.loadlive2d !== 'undefined') {
-          // https://github.com/xiazeyu/live2d-widget-models
-          try {
-            loadlive2d('live2d', petLink)
-          } catch (error) {
-            console.error('读取PET模型', error)
-          }
-        }
-      })
-    }
-  }, [theme])
+  // useEffect(() => {
+  //   if (showPet && !isMobile()) {
+  //     Promise.all([
+  //       loadExternalResource(
+  //         'https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js',
+  //         'js'
+  //       )
+  //     ]).then(e => {
+  //       if (typeof window?.loadlive2d !== 'undefined') {
+  //         // https://github.com/xiazeyu/live2d-widget-models
+  //         try {
+  //           loadlive2d('live2d', petLink)
+  //         } catch (error) {
+  //           console.error('读取PET模型', error)
+  //         }
+  //       }
+  //     })
+  //   }
+  // }, [theme])
 
-  function handleClick() {
-    if (petSwitchTheme) {
-      switchTheme()
-    }
-  }
+  // function handleClick() {
+  //   if (petSwitchTheme) {
+  //     switchTheme()
+  //   }
+  // }
 
-  if (!showPet) {
-    return <></>
-  }
+  // if (!showPet) {
+  //   return <></>
+  // }
 
   return (
-    <canvas
-      id='live2d'
-      width='280'
-      height='250'
-      onClick={handleClick}
-      className='cursor-grab'
-      onMouseDown={e => e.target.classList.add('cursor-grabbing')}
-      onMouseUp={e => e.target.classList.remove('cursor-grabbing')}
-    />
+    <></>
   )
 }
